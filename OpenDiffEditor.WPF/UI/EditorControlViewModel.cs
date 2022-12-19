625cec2ac4e9f442ae7b22b0228ce5d25383a17d
nanagami1369 <nanagami1369@gmail.com>
2022-12-19 21:17:56 +0900

    WPFで使用したOSSのライセンスを表示できるようにした

--- OpenDiffEditor.WPF/UI/EditorControlViewModel.cs
+++ OpenDiffEditor.WPF/UI/EditorControlViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using OpenDiffEditor.Common.Factory;
@@ -52,6 +53,7 @@ public class EditorControlViewModel : ObservableObject
     public IRelayCommand<string> DropOldRootPathCommand { get; }
     public IRelayCommand<string> DropNewRootPathCommand { get; }
     public IRelayCommand<DiffFileInfo> OpenVsCodeCommand { get; }
+    public IRelayCommand UsedLicenseCommand { get; }
 
     public EditorControlViewModel()
     {
@@ -100,6 +102,21 @@ public class EditorControlViewModel : ObservableObject
                 };
                 Process.Start(processStartInfo);
             });
+        });
+        UsedLicenseCommand = new RelayCommand(() =>
+        {
+            var message = @".NET Community Toolkit
+            Copyright © .NET Foundation and Contributors
+
+All rights reserved.
+            MIT License(MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+            ";
+            MessageBox.Show(message);
         });
     }
 
